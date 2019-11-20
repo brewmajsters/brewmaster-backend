1c4de9907ded051f5dd12c924e636706f00bd485
Adam Žúrek <adamzurek14@gmail.com>
2019-11-20 17:20:19 +0100

    - **Feature**: Implemented test scenario for sending async data to FE.

---
+++ static/test.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    //connect to the socket server.
+    var socket = io.connect(
+        'http://' + document.domain + ':' + location.port + '/test_web_socket',
+        {transports: ['websocket', 'polling', 'flashsocket']}
+    );
+    var numbers_received = [];
+
+    //receive details from server
+    socket.on('new_number', function (msg) {
+        console.log("Received number" + msg.number);
+        //maintain a list of ten numbers
+        if (numbers_received.length >= 10) {
+            numbers_received.shift()
+        }
+        numbers_received.push(msg.number);
+        numbers_string = '';
+        for (var i = 0; i < numbers_received.length; i++) {
+            numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
+        }
+        $('#log').html(numbers_string);
+    });
+});
